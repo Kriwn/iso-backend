235c31a28c3da8cd98c98164618bfc3c080c36c7
Kriwn <nonisgod@gmail.com>
2025-12-25 21:27:53 +0800

    feat(prisma): create prisma client

---
+++ src/infrastructures/db/prisma/client.ts
@@ -0,0 +1,30 @@
+import { logger } from "../../../config/logger";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../../../generated/prisma/client";
+
+let prisma: PrismaClient | null = null;
+
+export function getPrismaClient() {
+  if (prisma) return prisma;
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  const adapter = new PrismaPg(pool);
+
+  prisma = new PrismaClient({
+    adapter,
+    log: ["error", "info", "warn"],
+  });
+
+  prisma.$connect()
+    .then(() => logger.info("Connected to PostgreSQL via Prisma"))
+    .catch((err: unknown) => {
+      logger.error("Prisma connection failed", { err });
+      process.exit(1);
+    });
+
+  return prisma;
+}
