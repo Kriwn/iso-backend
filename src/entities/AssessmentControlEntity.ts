a9c0b478bf87035b9bf681dbf66c7d3c4b43f7c8
Kriwn <nonisgod@gmail.com>
2026-01-19 00:01:23 +0800

    refactor(AssessmentControl): change parameter data type in fuction

--- src/entities/AssessmentControlEntity.ts
+++ src/entities/AssessmentControlEntity.ts
@@ -1,24 +1,27 @@
+import { AssessmentControl } from "../../generated/prisma/client";
+
 export type AssessmentControlType = "PHYSICAL" | "PEOPLE" | "ORGANIZATION" | "TECHNOLOGICAL";
 
 export interface AssessmentControlEntity {
 	id: number;
 	assessmentId: number;
-	controlId: number;
-	status: AssessmentControlType;
-	evidenceUrl?: string | null;
-	comments?: string | null;
+	count: number;
+	maxCount: number;
+	context: string;
+	type: AssessmentControlType;
 	createdAt: Date;
 	updatedAt: Date;
 }
 
-export function toAssessmentControlEntity(data: any): AssessmentControlEntity {
+
+export function toAssessmentControlEntity(data: AssessmentControl): AssessmentControlEntity {
 	return {
 		id: data.id,
-		assessmentId: data.assessmentId,
-		controlId: data.controlId,
-		status: data.status,
-		evidenceUrl: data.evidenceUrl,
-		comments: data.comments,
+		assessmentId: data.isoAssessmentId,
+		count: data.count,
+		maxCount: data.maxCount,
+		context: data.context,
+		type: data.type,
 		createdAt: data.createdAt,
 		updatedAt: data.updatedAt,
 	};
