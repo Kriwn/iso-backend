74f52935501a6a825cbf0c9ea1e1adf4cc02e0e9
Kriwn <nonisgod@gmail.com>
2026-01-25 01:31:14 +0800

    fix(git):path file bug

--- src/errors/AssessmentControlError.ts
+++
@@ -1,6 +0,0 @@
-export class AssessmentControlNotFoundError extends Error {
-	constructor(id: number) {
-		super(`AssessmentControl with ID ${id} not found`);
-		this.name = "AssessmentControlNotFoundError";
-	}
-}
